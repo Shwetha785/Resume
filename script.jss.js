02bd9790da983c23bf2270f0a020fa633ad82430
Shwetha785 <bshwethabhat@gmail.com>
2024-12-07 08:23:56 +0530

    Add files via upload

---
+++ script.jss.js
@@ -0,0 +1,7 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function() {
+    const greetingElement = document.getElementById("greeting");
+    greetingElement.innerText = "Hello, welcome to my website!";
+});
+
+
